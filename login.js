1a1c74a0f496ed31d5dbee58cdf04e10e0694306
John-Bush14 <joppe@stuyck.be>
2024-10-17 23:45:01 +0200

    feat: auto login + auto credential-getting

--- login.js
+++ login.js
@@ -1,1 +1,45 @@
-console.log("dd")
+if (chrome != undefined) {browser = chrome;}
+
+
+const formData = new URLSearchParams();
+
+
+let submit_button = document.querySelector("#submit_button");
+
+submit_button.type = "";
+
+let form = submit_button.parentElement.parentElement;
+
+
+browser.storage.local.get(['username', 'password'], function(config) {
+   if (config.username == undefined || config.password == undefined || config.username == "" || config.password == "") {
+      async function wait_for_submit() {await new Promise((resolve) => {submit_button.addEventListener('click', () => {resolve();});})};
+      wait_for_submit();
+   
+      config.username = form.children[0].children[1].value;
+      config.password = form.children[1].children[1].value;
+
+      browser.storage.local.set(config);
+
+      return;
+   }
+
+   formData.append('username', config.username);
+   formData.append('password', config.password);
+
+
+   fetch(window.location.href, {
+      method: "POST",
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}, // don't ask me man ask chat-gpt
+      body: formData.toString(),
+   })
+   .then(response => {
+      if (response.ok) {console.log('Login response successful')} 
+      else {
+         console.log('Login response failed', response.status)
+      }
+   })
+   .catch(error => {
+      console.error('Error posting login:', error);
+   });
+});
